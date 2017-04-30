24937b71529e9ec9c9f4355cb114a43f26fe1eb3
mosimchah <mosimchah@gmail.com>
2017-04-29 23:40:05 -0400

    Revert "blue active"
    This reverts commit b44df16bf20786f564be74c11c1ab75913e38537.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -549,9 +549,6 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
-extern struct cpufreq_governor cpufreq_gov_blu_active;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
@@ -561,7 +558,6 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
->>>>>>> b004458... add Bioshock gov
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
